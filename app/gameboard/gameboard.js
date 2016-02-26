1312f9de8ee861a71c4641eff97b6e0ef6263478
Charlie Li <ok>
2016-02-26 02:32:06 -0500

    Got mancala board set up

--- app/gameboard/gameboard.js
+++ app/gameboard/gameboard.js
@@ -11,5 +11,52 @@ angular.module('myApp.gameboard', ['ngRoute'])
 
 .controller('MancalaBoardCtrl', ['$scope','GameLogic', function($scope, GameLogic) {
   console.log(GameLogic)
-  $scope.helloWorld = "Hellow World";
+
+  $scope.gameSettings = {
+    numberOfStones: 6,
+    mancalaHole:6,
+    mancalaHoleArray: function() {
+
+      return new Array($scope.gameSettings.mancalaHole)
+    }
+  };
+
+  $scope.gameBoardEvents = {
+    gameStonesChanged: function() {
+      console.log($scope.numberOfStones);
+    },
+    gameHolesChanged: function() {
+      console.log("Rebuilding board")
+      //$scope.$apply(function(){
+      //  $scope.gameSettings.mancalaHoleArray()
+      //});
+    }
+  };
+
+  $scope.gameHoleStates = {
+    blueHoles: function(){
+
+    },
+    redHoles: function() {
+
+    }
+  }
+
+  $scope.getGameUIStates = {
+    blueMancalaHoles: function() {
+      var holes = new Array($scope.gameSettings.mancalaHole);
+      holes[4] = 2;
+      return holes;
+    },
+    redMancalaHoles: function() {
+      var holes = new Array($scope.gameSettings.mancalaHole);
+      holes[0]= 14;
+      return holes;
+    }
+  }
+
+  $scope.cellClicked = function(playerColor, cellNumber) {
+    console.log(playerColor)
+    console.log(cellNumber)
+  }
 }]);
